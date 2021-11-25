e0c399661c15005b344e8f3da00272cc1b57c468
SimoneSalzedo <s.salzedo@studenti.poliba.it>
2021-11-25 22:27:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projectalexandria/HomeActivity.java
@@ -0,0 +1,61 @@
+package com.example.projectalexandria;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarMenuView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        };
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Fragment1()).commit();
+
+        bottomNavigationView =findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.item1:
+                        fragment= new Fragment1();
+                        break;
+                    case R.id.item2:
+                        fragment= new Fragment2();
+                        break;
+                    case R.id.item3:
+                        fragment= new Fragment3();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+
+                return true;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/projectalexandria/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.projectalexandria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        };
+
+        Runnable fadingEffect = new Runnable() {
+            @Override
+            public void run() {
+                fade();
+            }
+        };
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(fadingEffect);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this , HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+
+    public void fade(){
+    ImageView imageView = findViewById(R.id.farologo);
+    Animation animationfade = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+    imageView.startAnimation(animationfade);
+    }
+
+}
