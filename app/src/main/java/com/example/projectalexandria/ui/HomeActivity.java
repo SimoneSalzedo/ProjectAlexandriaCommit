de8417c9302df419ebc441b0fc81aa102d821307
fSidel <93013110+fSidel@users.noreply.github.com>
2021-12-08 11:39:01 +0100

    Ripulire i permessi (Cattiva indentazione)

--- app/src/main/java/com/example/projectalexandria/ui/HomeActivity.java
+++ app/src/main/java/com/example/projectalexandria/ui/HomeActivity.java
@@ -31,26 +31,27 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         //PERMISSION REQUEST
-            ActivityResultLauncher<String> requestPermissionReadExt =
-                    registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                        if (!isGranted) {
-                            System.out.println("Ma che stai a fa'");
-                            Toast.makeText(this, R.string.ToastMsgStoragePermissionDenied, Toast.LENGTH_LONG).show();
-                        }
-                    });
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissionReadExt.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
-            }
+
+        ActivityResultLauncher<String> requestPermissionReadExt =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                    if (!isGranted) {
+                        Toast.makeText(this, R.string.ToastMsgStoragePermissionDenied, Toast.LENGTH_LONG).show();
+                    }
+                });
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionReadExt.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
         ActivityResultLauncher<String> requestPermissionWriteExt =
                 registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                     if (!isGranted) {
-                        System.out.println("Ma che stai a fa'");
                         Toast.makeText(this, R.string.ToastMsgStoragePermissionDenied, Toast.LENGTH_LONG).show();
                     }
                 });
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermissionWriteExt.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
+
         //END PERMISSION REQUEST
     }
 
