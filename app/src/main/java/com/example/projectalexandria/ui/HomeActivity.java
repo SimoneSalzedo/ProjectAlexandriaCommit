871f9b4194c40f5a1926ce23009647ff2bd28697
fSidel <93013110+fSidel@users.noreply.github.com>
2021-12-08 11:16:49 +0100

    Tentativo di refactoring della UI

--- app/src/main/java/com/example/projectalexandria/ui/HomeActivity.java
+++ app/src/main/java/com/example/projectalexandria/ui/HomeActivity.java
@@ -20,65 +20,16 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.projectalexandria.R;
-import com.example.projectalexandria.data.database.ShelfEntry;
-import com.example.projectalexandria.ui.fragments.Fragment1;
-import com.example.projectalexandria.ui.fragments.Fragment2;
-import com.example.projectalexandria.ui.fragments.Fragment3;
 import com.example.projectalexandria.ui.viemodel.ShelfViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import LibraryServices.FileUtil;
-
 public class HomeActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
-    ShelfViewModel shelfViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        shelfViewModel = new ViewModelProvider(this).get(ShelfViewModel.class);
-        shelfViewModel.getAllEntries().observe(this, new Observer<List<ShelfEntry>>() {
-            @Override
-            public void onChanged(List<ShelfEntry> shelfEntries) {
-                // update recycleview
-            }
-        });
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
-        }
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Fragment1()).commit();
-
-        bottomNavigationView =findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-
-                switch (item.getItemId()){
-                    case R.id.item1:
-                        fragment= new Fragment1();
-                        break;
-                    case R.id.item2:
-                        fragment= new Fragment2();
-                        break;
-                    case R.id.item3:
-                        fragment= new Fragment3();
-                        break;
-                }
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
-                return true;
-            }
-
-        });
         //PERMISSION REQUEST
             ActivityResultLauncher<String> requestPermissionReadExt =
                     registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
@@ -103,20 +54,5 @@ public class HomeActivity extends AppCompatActivity {
         //END PERMISSION REQUEST
     }
 
-    public void onActivityResult(int requestcode, int resulCode, Intent data) {
-        super.onActivityResult(requestcode,resulCode,data);
-        if (requestcode == requestcode && resulCode == Activity.RESULT_OK) {
-            if (data == null) {
-                return;
-            } Uri uri = data.getData();
-            System.out.println(uri.toString());
-            try {
-                File inputFile = FileUtil.from(HomeActivity.this, uri);
-                Log.d("file", "File...:::: uti - "+inputFile .getPath()+" file -" + inputFile + " : " + inputFile .exists());
-                FileUtil.writeToSDFile( HomeActivity.this, inputFile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            }
-    }
+
     }
